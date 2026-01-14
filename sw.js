bfde7f1c7239810f186122cb2d5edc4743573902
Szr80 <sezerytbe80@gmail.com>
2026-01-14 16:54:18 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
-self.addEventListener('fetch', function(event) {
-    // Uygulamanın çevrimdışı çalışabilmesi için temel yapı
+self.addEventListener('push', function(event) {
+    const options = {
+        body: event.data.text(),
+        icon: 'https://cdn-icons-png.flaticon.com/512/3103/3103611.png',
+        vibrate: [100, 50, 100],
+        data: { dateOfArrival: Date.now() }
+    };
+    event.waitUntil(
+        self.registration.showNotification('Yeni Mesaj!', options)
+    );
 });
