28cad1330b190df70933b356999a5102cb3a68ae
Szr80 <sezerytbe80@gmail.com>
2026-01-14 15:48:55 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,27 +1,18 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
-// Firebase Ayarların (Yukarıdakiyle Aynı Olmalı)
 firebase.initializeApp({
-    apiKey: "AIzaSy...",
-    authDomain: "projen.firebaseapp.com",
-    projectId: "projen-id",
-    storageBucket: "projen.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:12345:web:abcde"
+    apiKey: "BURAYA_API_KEY",
+    projectId: "PROJE_ID",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
 });
 
 const messaging = firebase.messaging();
 
-// Arka planda mesaj gelince ne olacak?
 messaging.onBackgroundMessage((payload) => {
-    console.log('Arka plan mesajı alındı: ', payload);
-
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
+    self.registration.showNotification(payload.notification.title, {
         body: payload.notification.body,
         icon: 'https://cdn-icons-png.flaticon.com/512/2088/2088617.png'
-    };
-
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    });
 });
